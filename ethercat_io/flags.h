514fb9300b18501f67dec842848263e29396f251
Tiloup9291 <158225974+Tiloup9291@users.noreply.github.com>
2026-02-27 22:39:09 -0500

    Delete ethercat_io directory

--- ethercat_io/flags.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FLAGS_H_
-#define _FLAGS_H_
-
-#include <stdatomic.h>
-#include <stdint.h>
-
-extern _Atomic uint32_t run_enable;
-
-#endif /* _FLAGS_H_ */
--- ethercat_io/service.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SERVICE_H_
-#define _SERVICE_H_
-
-int service(void);
-
-#endif /* _SERVICE_H_ */
